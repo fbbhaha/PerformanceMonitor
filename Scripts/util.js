d7aa27c64be4a99e74e3ed8737acf771824e4661
fbb <1191447463@qq.com>
2020-02-10 13:31:05 +0800

    2.10

--- Scripts/util.js
+++ Scripts/util.js
@@ -1,6 +1,17 @@
 var readearth = {
-  host: "http://192.168.2.201:9999"
+  //host: "http://192.168.2.201:9999",
+    host: "http://readearth2014.vicp.cc:20010",
+  uId: 0
 };
+
+var tkType = {
+  cpu:1,
+  mem:2,
+  sys:3,
+  sysfile:4,
+  db:5,
+  conn:6
+}
 
 
 Date.prototype.format = function(fmt) {
@@ -35,18 +46,42 @@ function GetQueryString(name) {
 
 function initAjaxLoad() {
   $.ajaxSetup({
+      xhrFields: {
+        withCredentials: true
+      },
+      crossDomain: true,
       beforeSend: function() {
           var indexArray = [];
-          this.layindex = layer.load(2, { shade: [0.3, '#000'] });
+          this.layindex = parent.layer.load(2, { shade: [0.3, '#000'] });
           if (!window.indexArray)
               window.indexArray = indexArray;
           window.indexArray.push(this.layindex);
       },
-      complete: function() {
-          if (window.indexArray) {
-              layui.layer.close(window.indexArray[window.indexArray.length - 1]);
+      complete: function(res) {
+          if (window.indexArray||res.code==500) {
+              parent.layui.layer.close(window.indexArray[window.indexArray.length - 1]);
               window.indexArray.length = window.indexArray.length - 1;
           }
       }
   });
+}
+
+function setCookie(key, value, iDay) {
+	var oDate = new Date();
+	oDate.setDate(oDate.getDate() + iDay);
+	document.cookie = key + '=' + value + ';expires=' + oDate+ ';path='+'/';
+}
+
+function removeCookie(key) {
+	setCookie(key, '', -1);//这里只需要把Cookie保质期退回一天便可以删除
+}
+function getCookie(key) {
+  var cookieArr = document.cookie.split('; ');
+	for(var i = 0; i < cookieArr.length; i++) {
+		var arr = cookieArr[i].split('=');
+		if(arr[0] === key) {
+			return arr[1];
+		}
+	}
+	return false;
 }
